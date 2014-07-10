7e929f6d1c2adf1af566fb43ecd6b7a0d26579fb
Robby Wauters <robbywauters@gmail.com>
2014-07-10 17:52:45 +0200

    Properties panel (click entity to see props)

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -12,25 +12,28 @@ var ManualDirector = function (options){
 		$("ramp-shop-entity").on('click', function(){
 			//console.log($(this).attr("type"));
         	$("#topView").append("<ramp-entity type='"+$(this).attr("type")+"' x='0' y='0' direction='down'></ramp-entity>");
-        	$("ramp-entity").on('mouseup', checkDirections);
-        	$("ramp-entity").on('touchend', checkDirections);
+        	$("ramp-entity").last().on('mouseup', checkDirections);
+        	$("ramp-entity").last().on('touchend', checkDirections);
+        	$("ramp-entity").last().on('click', function(){
+	        	showProperties($(this));
+	        });
         });
-        //console.log($("ramp-entity").first());
+        $("ramp-entity").on('click', function(){
+        	showProperties($(this));
+        });
 
         $("ramp-entity").on('mouseup', checkDirections);
         $("ramp-entity").on('touchend', checkDirections);
 	};
 
 	var checkDirections = function(event){
+		event.preventDefault();
 		//tables
 		var table = $("ramp-entity[type='table']").first();
 
 		//console.log($(this));
 
 		$(this).attr("direction",entityPosToTable($(this),table));
-
-		//console.log(event);
-		//console.log($(this).attr("x"));
 	};
 
 	var entityPosToTable = function(entity, table){
@@ -66,6 +69,19 @@ var ManualDirector = function (options){
 
 		return "";
 	}
+
+	var showProperties = function(entity){
+		console.log(entity[0]["imgName"]);
+		$(".typeTitle").text(entity[0]["type"]);
+		$(".propertyPane img").attr("src", "images/"+entity[0]["imgName"]+".png");
+		//$("select[name='micID']").val(entity[0]["x"]);
+		//$("select[name='onSeat']").val(entity[0]["x"]);
+		$("select[name='x']").val(entity[0]["x"]);
+		$("select[name='y']").val(entity[0]["y"]);
+		$("select[name='w']").val(entity[0]["w"]);
+		$("select[name='h']").val(entity[0]["h"]);
+		$("select[name='direction']").val(entity[0]["direction"]);
+	};
 
 	return {
 		init: init
