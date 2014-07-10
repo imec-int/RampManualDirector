96e2ddd53931d1f3fe587b42e23022fdf7a13b02
Robby Wauters <robbywauters@gmail.com>
2014-07-10 12:54:42 +0200

    auto-direction towards table

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,11 +9,62 @@ var ManualDirector = function (options){
 	};
 
 	var addHandlers = function (){
-		$(".shopItem").on('click', function(){
-          $("#topView")
+		$("ramp-shop-entity").on('click', function(){
+			//console.log($(this).attr("type"));
+        	$("#topView").append("<ramp-entity type='"+$(this).attr("type")+"' x='0' y='0' direction='up'></ramp-entity>");
         });
+        //console.log($("ramp-entity").first());
+
+        $("ramp-entity").on('mouseup', checkDirections);
+        $("ramp-entity").on('touchend', checkDirections);
 
 	};
+
+	var checkDirections = function(event){
+		//tables
+		var table = $("ramp-entity[type='table']").first();
+
+		//console.log($(this));
+
+		$(this).attr("direction",entityPosToTable($(this),table));
+
+		//console.log(event);
+		//console.log($(this).attr("x"));
+	};
+
+	var entityPosToTable = function(entity, table){
+		// Help variables
+		var ent = {
+			x:entity[0]["x"],
+			y:entity[0]["y"]
+		};
+		var tab = {
+			x:table[0]["x"],
+			y:table[0]["y"],
+			w:table[0]["w"],
+			h:table[0]["h"]
+		};
+		// console.log(ent);
+		// console.log(tab);
+
+		// check top border
+		if(((ent.x >= tab.x) && (ent.x < tab.x+tab.w)) && (ent.y == tab.y-1))
+			return "down";
+
+		// check left border
+		if(((ent.y >= tab.y) && (ent.y < tab.y+tab.h)) && (ent.x == tab.x-1))
+			return "right";
+
+		// check right border
+		if(((ent.y >= tab.y) && (ent.y < tab.y+tab.h)) && (ent.x == tab.x+tab.w))
+			return "left";
+
+		// check bottom border
+		if(((ent.x >= tab.x) && (ent.x < tab.x+tab.w)) && (ent.y == tab.y+tab.h))
+			return "up";
+
+		return "";
+	}
 
 	return {
 		init: init
