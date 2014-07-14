f42907b5f491d0f357eb63fac6d656a8621f3b65
Robby Wauters <robbywauters@gmail.com>
2014-07-14 12:28:37 +0200

    scene generation from JSON

--- public/javascripts/viewer.js
+++ public/javascripts/viewer.js
@@ -4,23 +4,33 @@ var ManualDirector = function (options){
 	var activeRuleOptions = "";
 
 	var activeEntity;
-	var scene = {};
+
 
 	var init = function (){
 		addHandlers();
-
+		generateScene();
 	};
 
 	var addHandlers = function (){
 		// $("ramp-entity").on('mouseup', handleActive);
   //       $("ramp-entity").on('touchend', handleActive);
 
-
+  		this.scene = roomConfig;
+  		//console.log(this.scene);
 	};
 
 	var generateScene = function(){
-
-	});
+		console.log(this.scene);
+		if(this.scene){
+			var ent = null;
+			for (var i = 0; i < this.scene.entities.length; i++) {
+				ent = this.scene.entities[i];
+				$("#topView").append("<ramp-entity type='"+ent.type+"' micId='"+ent.micId+"' x='"+ent.x+"' y='"+ent.y+"' w='"+ent.w+"' h='"+ent.h+"' direction='"+ent.direction+"' onSeat='"+ent.onSeat+"'></ramp-entity>");
+			};
+		}else{
+			console.log("no scene detected");
+		}
+	};
 
 	var sceneToJSON = function(){
 		var entities = [$("ramp-entity").length];
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 var serverUrl = require('../config').serverUrl;
 var httpreq = require('httpreq');
 
-var currentRoomConfig = ["wadde","lol"];
+var currentRoomConfig = {"scene":"DirectorQ","entities":[{"type":"table","micId":"NA","x":2,"y":2,"w":5,"h":3,"direction":"down","onSeat":false},{"type":"member","micId":1,"x":3,"y":1,"w":1,"h":1,"direction":"down","onSeat":false},{"type":"member","micId":1,"x":4,"y":1,"w":1,"h":1,"direction":"down","onSeat":false},{"type":"member","micId":1,"x":5,"y":1,"w":1,"h":1,"direction":"down","onSeat":false}]};
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	res.render('index', { title: 'Manual Director - Q' , roomConfig: currentRoomConfig});
+	res.render('index', { title: 'Manual Director - Q' , roomConfig: JSON.stringify(currentRoomConfig)});
 
 });
 
